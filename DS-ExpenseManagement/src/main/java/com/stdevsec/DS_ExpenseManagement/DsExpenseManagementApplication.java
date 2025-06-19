a69161d4c203473efe7c56fd112bb3b79430577d
ilaktatoor <cemmnuelaxa@gmail.com>
2025-06-18 22:10:23 -0600

    first commit

---
+++ DS-ExpenseManagement/src/main/java/com/stdevsec/DS_ExpenseManagement/DsExpenseManagementApplication.java
@@ -0,0 +1,13 @@
+package com.stdevsec.DS_ExpenseManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsExpenseManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsExpenseManagementApplication.class, args);
+	}
+
+}
---
+++ DS-ExpenseManagement/src/test/java/com/stdevsec/DS_ExpenseManagement/DsExpenseManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stdevsec.DS_ExpenseManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsExpenseManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
